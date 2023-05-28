8a7263eb1b6528f33bfe24b475e5ff2a192971c3
169233 <benedikta.jana@seznam.cz>
2023-05-28 21:36:17 +0200

    add Footer with link to Github

--- src/App.js
+++ src/App.js
@@ -2,10 +2,15 @@ import './App.css';
 import Weather from './Weather';
 function App() {
   return (
-    <div className="App">
-            <Weather/>
+  <div>
+      <div className="App">
+      <Weather />
     </div>
-  );
+    <div className="Footer">
+        <a className="Link" href="https://github.com/169233/react-app" target="_blank" rel="noreferrer">Open source </a><span>by Jana Srahulkova</span>
+      </div>
+  </div>
+    );
 }
 
 export default App;
